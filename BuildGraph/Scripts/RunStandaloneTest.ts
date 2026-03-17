e6ef6c96c705a00044531ad72a067181ae95eed6
Maxim Devoir <maximdevoir@gmail.com>
2026-03-17 15:37:32 -0700

    feat: add dedicated, listen server, PIE, and standalone test scripts

---
+++ BuildGraph/Scripts/RunStandaloneTest.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import {ATO, Orchestrator, OrchestratorMode} from '@maximdevoir/ato';
+
+const ATCStandaloneTest = ATO.fromCommandLine();
+
+const standaloneOrchestrator = new Orchestrator(OrchestratorMode.Standalone);
+ATCStandaloneTest.addOrchestrator(standaloneOrchestrator);
+
+const code = await ATCStandaloneTest.start();
+process.exit(code);
